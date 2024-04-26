6f74c959dccbb93da3f09a50fbe73ab34aa23390
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-04-26 18:18:12 +0300

    small updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-
+import Home from './pages/home/Home.tsx';
+import Login from "./pages/login/Login.tsx";
 import './App.css';
-
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
       </Routes>
     </HashRouter>
   );
